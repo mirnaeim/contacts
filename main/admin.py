7a3a8cae82332e453bfcd46356a46d8749d82461
unknown <mirnaimhoseyni@gmail.com>
2024-02-14 22:57:57 +0330

    contact model created.

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.admin import register
+
+from .models import Contact
 
 # Register your models here.
+
+
+@register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'address', 'created_date', 'updated_date')
+    list_display_links = ('first_name', 'last_name')
+    list_filter = ('created_date', 'updated_date',)
+    search_fields = ('title', 'description')
+
--- main/models.py
+++ main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=255, null=False, blank=False, verbose_name='FirstName')
+    last_name = models.CharField(max_length=255, null=False, blank=False, verbose_name='LastName')
+    phone_number = models.CharField(max_length=255, null=False, blank=False, verbose_name='PhoneNumber')
+    address = models.TextField(null=False, blank=False, verbose_name='Address')
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='Created Date', )
+    updated_date = models.DateTimeField(auto_now=True, verbose_name='Updated Date', )
+
+    class Meta:
+        verbose_name = 'Contact'
+        verbose_name_plural = 'Contacts'
+        ordering = ('id',)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
